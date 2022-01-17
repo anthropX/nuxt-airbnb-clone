a6202555baa0634e8e792be6fd688f4ee7e780cf
Vimal Patel <94vimalpatel@gmail.com>
2022-01-17 22:38:20 +0530

    feat: wrap pages inside layout with header and footer
    fix: avoid problem of scripts (loading many times) inside an SPA
    
    refactor: clean up code using the Google Maps API

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="layout flex flex-col">
+    <header class="bg-red-600 text-white p-2">
+      <NuxtLink to="/">Airbnb</NuxtLink>
+    </header>
+    <Nuxt class="grow" />
+    <footer class="text-center">Copyright 2022</footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
+<style scoped>
+.grow {
+  flex-grow: 1;
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -5,9 +5,7 @@ export default {
     htmlAttrs: {
       lang: 'en',
     },
-    bodyAttrs: {
-      class: 'using-mouse',
-    },
+    bodyAttrs: {},
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
